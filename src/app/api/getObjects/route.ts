8bf0365432504fff27e28d51e07e1812b6a6eaa0
Ebanx3 <esteban2santos@gmail.com>
2023-05-29 19:34:31 -0300

    trying to fix dynamic server usage

--- src/app/api/getObjects/route.ts
+++ src/app/api/getObjects/route.ts
@@ -28,8 +28,7 @@ export async function GET(req: Request) {
         return NextResponse.json({ success: true, data: ress }, { status: 200 })
     }
     catch (error: any) {
-        console.log(error)
-        return NextResponse.json({ success: false, error: error.message })
+        return NextResponse.json({ success: false, error: error })
 
     }
 }
